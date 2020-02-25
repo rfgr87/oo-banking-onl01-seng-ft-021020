72e6b951d4964d57fcaac2e39f9656dfc2c016dc
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-25 20:24:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -37,12 +37,14 @@ class Transfer
  
     
   def execute_transaction
-    if self.sender.balance > self.amount
-      self.receiver.balance += self.amount
-      self.sender.balance -= self.amount
-      self.status = "complete"
-    else 
-      "Transaction rejected. Please check your account balance."
+    if self.status = "pending"
+      if self.sender.balance > self.amount
+        self.receiver.balance += self.amount
+        self.sender.balance -= self.amount
+        self.status = "complete"
+      else 
+        "Transaction rejected. Please check your account balance."
+      end
     end
   end
   
