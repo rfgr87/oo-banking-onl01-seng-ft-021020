ae9f10e8dd78a05ba97ee49bc88633d9663ec2dd
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-25 18:57:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,21 @@ class Transfer
           @valid_sender = true 
         end 
       end 
-    end
     
+    BankAccount.all.select do |x|
+      if x.name == self.receiver 
+        if x.status == "open"
+          @valid_receiver = true 
+        end 
+      end 
+    end
+    if @valide_receiver == true && @valid_sender == true 
+      true 
+    else 
+      false 
+    end 
+  end
+    
+  
 
 end
