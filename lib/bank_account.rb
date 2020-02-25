ac1ae8b289b6db4872a2cec435af4d96052ac2c1
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-25 18:53:01 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,12 @@
 class BankAccount
+  @@all = []
   attr_accessor :name, :balance, :status
   
   def initialize(name, balance = 1000, status = "open")
     @name = name
     @balance = 1000
     @status = status
+    @@all << self
   end
   
   def deposit(money)
